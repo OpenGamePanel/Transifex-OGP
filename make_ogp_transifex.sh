8a1b774e20329eef13169c602b7021d1d1546037
own3mall <own3mall@gmail.com>
2017-11-29 19:37:06 -0700

    Latest Version

--- make_ogp_transifex.sh
+++ make_ogp_transifex.sh
@@ -1,23 +1,72 @@
 #!/bin/bash
 # Author:  own3mall <own3mall@gmail.com>
 # About:  Script that pulls the transifex files, puts them in the OGP stucture, copies the structure to the website and every module folder, runs a git pull, git clean, git commit, git push to remote repositories :D
+# Optional parameter ($1) is any new language to add
+
+######################
+#   IMPORTANT VARS   #
+######################
+
+transifexUser="{YOUR_TRANSIFEX_USER_HERE}"
+transifexPass="{YOUR_TRANSIFEX_PASS_HERE}"
+linuxUser="{YOUR_LINUX_USERNAME_HERE}"
 
 ######################
 #  FUNCTIONS #########
 ######################
 function setGlobalVars(){
+	# $1 is new languages string to add
+	# $2 is new language short code to add
 	# Variables
-	transifexDir="/home/own3mall/transifex"
-	gitDir="/home/own3mall/github"
-	logFile="/home/own3mall/transifex/logFile"
+	transifexDir="/home/${linuxUser}/transifex"
+	gitDir="/home/${linuxUser}/github"
+	currentRunDate=$(date "+%m_%d_%Y_%H-%M-%S-%p")
+	logFile="/home/${linuxUser}/transifex/logFile_${currentRunDate}"
 	gitPath="/usr/bin/git"
-	transifexUser="{YOUR_TRANSIFEX_USER_HERE}"
-	transifexPass="{YOUR_TRANSIFEX_PASS_HERE}"
+	BaseLang="English"
+	LangFolder="lang"
+	ClearLogFilesOlderThanDays=20
+	debug=true
+	LANGToAdd="$1"
+	LANGToAddCode="$2"
+	LANGCodesFile="/home/${linuxUser}/langCodes"
+	
+	# Adjust log file to contain new language in file name to distinguish the log file types
+	if [ ! -z "$LANGToAdd" ] && [ ! -z "$LANGToAddCode" ]; then
+		logFile="${logFile}_new_LANG_${LANGToAdd}_${LANGToAddCode}"
+	fi
+	
+	# Create the log file
+	> "${logFile}"
+	
+	# Save new language mappings to a file
+	WriteNewLanguageCodeAndStrIfNeeded
+	
+	# Load our saved language mappings
+	if [ -e "$LANGCodesFile" ]; then
+		source "$LANGCodesFile"
+	fi
 }
 
-function clearLogFile(){
-	# New log file
-	> "$logFile"
+function WriteNewLanguageCodeAndStrIfNeeded(){
+	if [ ! -z "$LANGToAdd" ] && [ ! -z "$LANGToAddCode" ]; then
+		# add it to our map file
+		# example: ar=Arabic
+		# Then source file and we have variables and values
+		if [ ! -e "$LANGCodesFile" ]; then
+			touch "$LANGCodesFile"
+		fi
+		
+		contents=$(cat "$LANGCodesFile")
+		hasLangCode=$(echo "$contents" | grep -o "${LANGToAddCode}=")
+		if [ -z "$hasLangCode" ]; then
+			echo "${LANGToAddCode}=${LANGToAdd}" >> "$LANGCodesFile"
+		fi
+	fi
+}
+
+function clearLogFilesOlderThan(){
+	find "$transifexDir" -mindepth 1 -type f -name "logFile*" -mtime +${ClearLogFilesOlderThanDays} -delete
 }
 
 function copyFile(){
@@ -48,66 +97,94 @@ function getTranslations(){
 function makeOGPStructure(){
 	cd "$transifexDir"
 	cd staging
+	
 	for folder in `find ./* -type d`; do
-		
-		OGPFileNamePRE=$(cut -d "." -f 3 <<< "$folder" | grep -o ".*[^php]")
+		# https://stackoverflow.com/questions/20348097/bash-extract-string-before-a-colon
+		# IN THIS CASE, EXTRACT STRING BEFORE ENDING "php"
+		OGPFileNamePRE=$(cut -d "." -f 3 <<< "$folder" | sed 's/php$//')
 		OGPFileNamePHP="${OGPFileNamePRE}.php"
-		
-		# Logging 
+
+		# Logging
 		logMessage "Current translation folder is $folder"
 		logMessage "OGP module name is $OGPFileNamePRE"
 		logMessage "OGP full language file name is $OGPFileNamePHP"
-		
+
 		if [ -e "$folder" ]; then
 			cd "$folder"
 			for f in `find ./* -type f | sed "s|^\./||"`; do
+				cpyFile=true
+				langPath=
 				case "$f" in
-				"en.php")
-					langPath="lang/English"
-					copyFile
-					;;
-				"da.php")
-					langPath="lang/Danish"
-					copyFile
-					;;
-				"de.php")
-					langPath="lang/German"
-					copyFile
-					;;
-				"es.php")
-					langPath="lang/Spanish"
-					copyFile
-					;;
-				"fr.php")
-					langPath="lang/French"
-					copyFile
-					;;
-				"hu.php")
-					langPath="lang/Hungarian"
-					copyFile
-					;;
-				"pl.php")
-					langPath="lang/Polish"
-					copyFile
-					;;
-				"pt.php")
-					langPath="lang/Portuguese"
-					copyFile
-					;;
-				"ru.php")
-					langPath="lang/Russian"
-					copyFile
-					;;
+					"en.php")
+						langPath="lang/English"
+						;;
+					"da.php")
+						langPath="lang/Danish"
+						;;
+					"de.php")
+						langPath="lang/German"
+                        ;;
+					"es.php")
+						langPath="lang/Spanish"
+                        ;;
+					"fr.php")
+						langPath="lang/French"
+						;;
+					"hu.php")
+						langPath="lang/Hungarian"
+						;;
+					"pl.php")
+						langPath="lang/Polish"
+						;;
+					"pt.php")
+						langPath="lang/Portuguese"
+						;;
+					"ru.php")
+						langPath="lang/Russian"
+						;;
+					"it.php")
+						langPath="lang/Italian"
+						;;
+					"ar.php")
+						langPath="lang/Arabic"
+						;;
+					"${LANGToAddCode}.php")
+						langPath="lang/${LANGToAdd}"
+						;;
+					*)
+						# Check and see if it's defined in our lang code file
+						fileNameLangCode="$(returnUntilLastMatch "$f" ".")"
+						if [ ! -z "${!fileNameLangCode}" ]; then
+							langPath="lang/${!fileNameLangCode}"
+						else
+							cpyFile=false
+						fi
+						;;
 				esac
+				
+				# copy the files over if needed based on language
+				if [ "$cpyFile" = true ] && [ ! -z "$langPath" ]; then
+					copyFile
+				fi
+				
 			done
 			cd ..
 		fi
 	done
 }
 
+function returnUntilLastMatch(){
+	# $1 = string
+	# $2 = character
+	echo "${1%${2}*}"
+}
+
 function logMessage(){
 	if [ ! -z "$1" ]; then
 		echo -e "$1" >> "$logFile"
+		if [ "$debug" = true ]; then
+			echo -e "$1"
+		fi
 	fi
 }
 
@@ -118,21 +195,62 @@ function copyOGPStructureToAllGitRepos(){
 		for folder in `find "${gitDir}" -mindepth 1 -maxdepth 1 -type d`; do
 			logMessage "Current github folder is $folder"
 			cd "$folder"
+                        
+			# Get latest files from GitHub repo
 			"$gitPath" pull
+			
+			# Clean non-versioned files (in case there's some already in here that should no longer be here)
+			# https://stackoverflow.com/questions/5879932/git-clean-not-working-recursively (added -d flag0
+			"$gitPath" clean -df
+                        
+			# Search for existing files within the BaseLang folder so we know how to handle
+			# any possible new language files once we copy the mess of all module language files into this directory
+			found=$(find ${LangFolder}/${BaseLang} -type f | sed -e "s,^${LangFolder}/${BaseLang}/,,";)
+                        
+			# Copy our bundled language files which includes languages for everything in this repo / module
 			cp -rf "$transifexDir/staging/lang" "$folder"
-			"$gitPath" clean -f
+                        
+			# Handle adding new language files if any to the repo
+			if [ ! -z "${found}" ]; then
+				logMessage "Found this list of files that exist in the base master language of ${BaseLang} for this git repository (${pWD}):"
+				logMessage $(echo ${found})
+				handleNewLanguages "${found}"
+			fi
+                        
+			# Clean non-versioned files (those not under source control via the Git repo) and commit changes
+			"$gitPath" clean -df
 			"$gitPath" commit -am "Transifex Language Updates - Generated by own3mall's auto script."
 			"$gitPath" push
 		done
 	fi
 }
 
+function handleNewLanguages(){
+	if [ ! -z "$LANGToAdd" ] && [ ! -z "$1" ]; then
+		logMessage "We have new languages to add!"
+		pWD=$(pwd)
+		for var in "${LANGToAdd[@]}"
+		do
+			for i in `echo ${1}`
+			do
+				if [ -e "${LangFolder}/${var}/${i}" ]; then
+					logMessage "Adding file ${LangFolder}/${var}/${i} to GitHub repo ${pWD}!"
+					"$gitPath" add "${LangFolder}/${var}/${i}"
+				else
+					logMessage "File ${i} not found in ${LangFolder}/${var} for GitHub repo ${pWD}!  Skipping..."
+				fi
+			done
+		done
+	else
+		logMessage "No languages need to be added."
+	fi
+}
+
 ######################
 #  Main ##############
 ######################
-setGlobalVars
-clearLogFile
+setGlobalVars "$1" "$2"
+clearLogFilesOlderThan
 getTranslations
 makeOGPStructure
 copyOGPStructureToAllGitRepos
-
--- webserver.py
+++ webserver.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
-# Not actually sure if this works 100% yet, but another reason why it's in source control
+# Replace vars in brackets in order for this to work first
+# Example:  {PATH_TO_TRANSFIX_HOME_DIR} should be /home/user/transifex
+# Written by OwN-3m-All <own3mall@gmail.com>
+# Allows you to trigger the language sync whenever you want.
+# Make sure you configure git to use your GitHub key for all git operations
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
 import base64
@@ -11,70 +15,85 @@ import sys
 import subprocess
 
 class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+	def _set_headers(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/html')
+		self.end_headers()
 
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>Nothing to see here, no health pack!</h1></body></html>")
+	def do_GET(self):
+		self._set_headers()
+		passcode = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('ogpkey', None)
+		langToAdd = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('newlang', None)
+		langToAddCode = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('newlangCode', None)
+		if passcode != None:
+			print("Passcode received is %s from IP %s" % (str(passcode[0]), self.client_address))
+			if passcode[0] == "{YOUR_SECRET_TRIGGER_KEY_TO_PREVENT_JUST_ANYONE_FROM_RUNNING_THIS}":
+				print("Received valid user generated request from OGP team! Running script now...")
+				self.wfile.write("<html><head><title>OwN-3m-All's Transifex GitHub Integration Script</title></head><body><h1>Success! Running script now...</h1><h4>this will take a few minutes... check GitHub to see when complete.</h4></body></html>")
+				if langToAdd == None or langToAddCode == None:
+					command = 'nohup /bin/bash {PATH_TO_GET_OGP_LANG.SH}/make_ogp_transifex.sh > {PATH_TO_TRANSFIX_HOME_DIR}/logFile 2>&1 &'
+				else:
+					command = 'nohup /bin/bash {PATH_TO_TRANSFIX_HOME_DIR}/make_ogp_transifex.sh "' + langToAdd[0] + '" "' + langToAddCode[0] + '" > {PATH_TO_TRANSFIX_HOME_DIR}/logFile 2>&1 &'
+				output,error  = subprocess.Popen(
+					command, universal_newlines=True, shell=True,
+					stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+			else:
+				self.wfile.write("<html><body><h1>Nothing to see here, no health pack!</h1></body></html>")
 
-    def do_HEAD(self):
-        self._set_headers()
-        
-    def do_POST(self):
-        self._set_headers()
-        try:
-                hashVerification = str(self.headers['X-TX-Signature'])
-                print "Hash header received from transifex is %s." %hashVerification
-                
-                content = str(self.rfile.read(int(self.headers['Content-Length'])))
-                print "POSTed data content is %s." %content
-                
-                contentType = str(self.headers['Content-Type'])
-                print "Content type is %s." %contentType
-                if contentType == "multipart/form-data" or contentType == "application/x-www-form-urlencoded":
-                        post_data = urlparse.parse_qs(content)
-                        for key, value in post_data.iteritems():
-                                print "%s=%s" % (key, value)
-                        print "Value of param1 is %s" %post_data['param1'][0]
-                elif contentType == "application/json":
-                        dataObj = json.loads(content)
-                        data = str(dataObj)
-                        print "Raw json data is %s." %data
-                        h=hmac.new(key='{YOUR_TRANSIFEX_SECRET_KEY_HERE}', msg=data, digestmod=hashlib.sha1)
-                        h.hexdigest()
-                        hashedData=base64.b64encode(h.digest())
-                        if hashVerification == hashedData:
-                                print("Received valid transifex message!")							
-                                self.wfile.write("<html><body><h1>Message Received. Thanks!</h1></body></html>")
-                                command = 'nohup /bin/bash /home/own3mall/transifex/ogp_transifex.sh > /home/own3mall/transifex/logFile 2>&1 &'
-                                output,error  = subprocess.Popen(
-                                         command, universal_newlines=True, shell=True,
-                                         stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-                        else:
-                                print("Hash %s didn't match %s." % (hashedData, hashVerification))				                                    
-                                self.wfile.write("<html><body><h1>Invalid request received.  </h1></body></html>")
-                else:
-                        self.wfile.write("<html><body><h1>Invalid request received.</h1></body></html>")
-        except:
-			    self.wfile.write("<html><body><h1>Invalid request received.</h1></body></html>")
-        
-        
-def run(server_class=HTTPServer, handler_class=S, port={PORT_HERE}):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
-    sys.stderr = open('/home/own3mall/scripts/transifex_logfile_error.txt', 'w', 0)
-    sys.stdout = open('/home/own3mall/scripts/transifex_logfile_stdout.txt', 'w', 0)
-    sys.stdout.flush()
-    httpd.serve_forever()
+	def do_HEAD(self):
+		self._set_headers()
+
+	def do_POST(self):
+		self._set_headers()
+		try:
+			hashVerification = str(self.headers['X-TX-Signature'])
+			print "Hash header received from transifex is %s." %hashVerification
+
+			content = str(self.rfile.read(int(self.headers['Content-Length'])))
+			print "POSTed data content is %s." %content
+
+			contentType = str(self.headers['Content-Type'])
+			print "Content type is %s." %contentType
+			if contentType == "multipart/form-data" or contentType == "application/x-www-form-urlencoded":
+				post_data = urlparse.parse_qs(content)
+				for key, value in post_data.iteritems():
+					print "%s=%s" % (key, value)
+					print "Value of param1 is %s" %post_data['param1'][0]
+			elif contentType == "application/json":
+				dataObj = json.loads(content)
+				data = str(dataObj)
+				print "Raw json data is %s." %data
+				h=hmac.new(key='{YOUR_TRANSIFEX_SECRET_KEY_HERE}', msg=data, digestmod=hashlib.sha1)
+				h.hexdigest()
+				hashedData=base64.b64encode(h.digest())
+				if hashVerification == hashedData:
+					print("Received valid transifex message! Running script now..")
+					self.wfile.write("<html><body><h1>Message Received. Thanks!</h1></body></html>")
+					command = 'nohup /bin/bash {PATH_TO_TRANSFIX_HOME_DIR}/make_ogp_transifex.sh > {PATH_TO_TRANSFIX_HOME_DIR}/logFile 2>&1 &'
+					output,error  = subprocess.Popen(
+						command, universal_newlines=True, shell=True,
+						stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+				else:
+					print("Hash %s didn't match %s." % (hashedData, hashVerification))                                                              
+					self.wfile.write("<html><body><h1>Invalid request received.  </h1></body></html>")
+			else:
+				self.wfile.write("<html><body><h1>Invalid request received.</h1></body></html>")
+		except:
+			self.wfile.write("<html><body><h1>Invalid request received.</h1></body></html>")
+
+def run(server_class=HTTPServer, handler_class=S, port=57701):
+	server_address = ('', port)
+	httpd = server_class(server_address, handler_class)
+	print 'Starting httpd...'
+	sys.stderr = open('{LOG_LOCATION}/transifex_logfile_error.txt', 'w', 0)
+	sys.stdout = open('{LOG_LOCATION}/transifex_logfile_stdout.txt', 'w', 0)
+	sys.stdout.flush()
+	httpd.serve_forever()
 
 if __name__ == "__main__":
-    from sys import argv
+	from sys import argv
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+	if len(argv) == 2:
+		run(port=int(argv[1]))
+	else:
+		run()
